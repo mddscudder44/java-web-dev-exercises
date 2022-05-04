a3e649e5d68a2ed56146c16660455d6ebbc118d8
Maddie <mddscudder44@gmail.com>
2022-05-04 16:23:52 -0400

    Completed Chapter 8 Exercises - Interfaces & Polymorphism

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -1,18 +1,25 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        Comparator comparator = new FlavorComparator();
+        Comparator coneComparator = new ConeComparator();
+
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
+        flavors.sort(comparator);
 
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
+        cones.sort(coneComparator);
+        System.out.println(cones);
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
     }
